c7a3749334cb658d2875d7f9745be1fbf9d76e5f
watood <103485255+watood@users.noreply.github.com>
2022-05-21 12:45:42 +0800

    Update utils.py

--- cascade2p/utils.py
+++ cascade2p/utils.py
@@ -77,12 +77,11 @@ def define_model(filter_sizes,filter_numbers,dense_expansion,windowsize,loss_fun
   conv_filter = Conv1D
 
   outX = conv_filter(filter_numbers[0], filter_sizes[0], strides=1, activation='linear')(inputs)
-  outX = BatchNormalization()(outX)
   outX = conv_filter(filter_numbers[1], filter_sizes[1], activation='linear')(outX)
-  outX = BatchNormalization()(outX)
+
   outX = MaxPooling1D(2)(outX)
   outX = conv_filter(filter_numbers[2], filter_sizes[2], activation='linear')(outX)
-  outX = BatchNormalization()(outX)
+
   outX = MaxPooling1D(2)(outX)
   
   outX = Dense(dense_expansion, activation='linear')(outX) # 'linear' units work here as well!
