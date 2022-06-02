857a3f7419d56c45e809c40c108e1d4796095613
watood <103485255+watood@users.noreply.github.com>
2022-06-03 00:05:56 +0800

    Update cascade.py

--- cascade2p/cascade.py
+++ cascade2p/cascade.py
@@ -162,11 +162,12 @@ def train_model( model_name, model_folder='Pretrained_models', ground_truth_fold
             #                    optimizer = cfg['optimizer']
             #                            )
             
-            from tensorflow.keras.layers import Dense,LSTM,Conv1D,MaxPooling1D,TimeDistributed,Flatten
+            from tensorflow.keras.layers import Dense,LSTM,Conv1D,MaxPooling1D,TimeDistributed,Flatten,Input
             from tensorflow.keras.models import Sequential
            
             
             model = Sequential()
+            model.add(Input(shape=(64,1)))
             model.add(TimeDistributed(Conv1D(filters=30, kernel_size=31,strides=1, activation='relu',input_shape=(64,1))))
             model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
             model.add(TimeDistributed(Flatten()))
